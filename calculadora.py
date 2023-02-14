eecaea1ced2add040e4e25c606ea677953cbff82
Vitor Vouvouloudas de Moraes <vouvouloudas1@gmail.com>
2023-02-14 13:00:19 -0300

    Update calculadora.py
    Adicionei funções através do arquivo 'funcalc' para realizar os cálculos.

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,17 @@
+from funcalc import *
+
 while True:
     n1 = float(input('Insira o primeiro número: '))
     n2 = float(input('Insira o segundo número: '))
     operacao = str(input('Soma [+] / Subtração [-] / Multiplicação [*] / Divisão [/]\n')).strip().lower()
     if operacao == '+':
-        resultado = n1 + n2
-        print(f'{n1} + {n2} = {resultado}')
+        print(f'{n1} + {n2} = {soma(n1, n2)}')
     if operacao == '-':
-        resultado = n1 - n2
-        print(f'{n1} - {n2} = {resultado}')
+        print(f'{n1} - {n2} = {subtracao(n1, n2)}')
     if operacao == '*':
-        resultado = n1 * n2
-        print(f'{n1} * {n2} = {resultado}')
+        print(f'{n1} * {n2} = {multiplicacao(n1, n2)}')
     if operacao == '/':
-        resultado = n1 / n2
-        print(f'{n1} / {n2} = {resultado}')
+        print(f'{n1} / {n2} = {divisao(n1, n2)}')
     elif operacao not in '+-*/':
         print('Opção inválida!')
 
